87763722d4b1ac684538ad2509ac6903c54ce8cf
Laura Betti <bettilaura0@gamail.com>
2025-03-27 13:59:16 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
     <Header />
+    <br/>
+    <Footer />
       {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}/>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,39 @@
 //Style
-import "./styles/Footer.css"
+import "./styles/Footer.css";
 
-function Footer () {
-    return(
-        <>
-        <footer className="footerbox">
-            <section> 
-                
+//Imagens
+import foto1 from "../assets/gmail.png"
+import foto2 from "../assets/instagram.png"
+import logoG3 from "../assets/logoG3.png"
+
+function Footer() {
+  return (
+    <>
+      <footer className="footerBox">
+
+        <section className="contatos">
+            <section className="bloco-img1">
+                <img className="img-redes" src={foto1} />
+                <h3 className="email">abacate.grupo3@gmail.com</h3>
             </section>
-        </footer>
-        </>
-    )
+
+            <section className="bloco-img2">
+                <img className="img-redes" src={foto2} />
+                <h3 className="instagram">@grupo3_abacates</h3>
+             </section>
+        </section>
+
+        <img className="imglogo" src={logoG3} />
+
+        <section className="integrantes">
+            <p>
+            Laura Betti Migliaccio - Lucas Casagrande da Silva - Milena Oliveira Souza<br />
+            Pietro Melle Michelin - Pyetro Joaquim Nunes - Vitor Geraldo Cecato
+            </p>
+        </section>
+      </footer>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
