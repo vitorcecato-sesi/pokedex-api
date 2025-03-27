00b7603581da2ddc6f02ff90e9456a29dadb1e90
Lucas C <lucascasagrandesesi@gmail.com>
2025-03-27 13:53:50 -0300

    Correção de bugs e finalização do sistema de busca no home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,15 +20,15 @@ function Home() {
     if(busca) buscarPokemons()
   }, [busca])
 
-  const guardarInformacoes = (pokemon) => {
-    const informacoes = {
-      nome: pokemon.name,
-      imagem: pokemon.sprites.front_default,
-      tipo: pokemon.types[0].type.name,
-      habilidades: pokemon.abilities.map(abilidade => abilidade.ability.name),
-      estatistica: pokemon.stats.map(estatistica => estatistica.base_stat)
+  const guardarInformacoes = () => {
+    const dados = {
+      nome: pokemons.name,
+      imagem: pokemons.sprites.front_default,
+      tipo: pokemons.types[0].type.name,
+      habilidades: pokemons.abilities.map((abilidade) => abilidade.ability.name),
+      estatistica: pokemons.stats.map((estatistica) => estatistica.base_stat)
     }
-    setInformacoes(informacoes)
+    setInformacoes(dados)
     localStorage.setItem("Informações", JSON.stringify(informacoes))
   }
 
@@ -36,15 +36,12 @@ function Home() {
     localStorage.setItem("Dados API", JSON.stringify(pokemons))
   }, [pokemons])
 
-  const mudancaInput = (e) => {
-    setBusca(e.target.value)
-  }
 
   return(
     <>
       <section className="contIco">
-        <i className="fa-solid fa-magnifying-glass"></i>
-        <input className="pesquisar" value={busca} onChange={mudancaInput} placeholder="Insira ID ou nome de um pokémon"/>
+        <button onClick={guardarInformacoes} className="fa-solid fa-magnifying-glass"></button>
+        <input className="pesquisar" value={busca} onChange={(e) => setBusca(e.target.value)} placeholder="Insira ID ou nome de um pokémon"/>
       </section>
     </>
   )
