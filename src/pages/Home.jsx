55330a6a5948a484174efc97ae7a262a9974712c
Lucas C <lucascasagrandesesi@gmail.com>
2025-03-27 14:57:03 -0300

    Home alterações

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,10 @@ function Home() {
   const [pokemons, setPokemons] = useState(JSON.parse(localStorage.getItem("Dados API")) || {})
   const [busca, setBusca] = useState("")
   const [informacoes, setInformacoes] = useState(JSON.parse(localStorage.getItem("Informações")) || "")
+  const [erro, setErro] = useState(null);
+  const [loading, setLoading] = useState(true); 
+  const [mensagem, setMensagem] = useState ("")
+
 
   useEffect(() => {
     async function buscarPokemons() {
@@ -15,10 +19,13 @@ function Home() {
         setPokemons(dados)
       } catch (error) {
         console.error(error)
+        setErro(erro.message);
+      } finally {
+        setLoading(false);
       }
     }
     if(busca) buscarPokemons()
-  }, [busca])
+  }, [])
 
   const guardarInformacoes = () => {
     const dados = {
@@ -30,18 +37,34 @@ function Home() {
     }
     setInformacoes(dados)
     localStorage.setItem("Informações", JSON.stringify(informacoes))
+
   }
 
   useEffect(() => {
     localStorage.setItem("Dados API", JSON.stringify(pokemons))
   }, [pokemons])
 
+    if (erro) return setMensagem ("Pokémon não encontrado!")
+        
+    
+
+function mensagemLoading () {
+    if (loading) {
+        return (
+        <p>Carregando pokemons...</p>
+        )
+    }
+}
 
   return(
     <>
-      <section className="contIco">
-        <button onClick={guardarInformacoes} className="fa-solid fa-magnifying-glass"></button>
+
+    <h2>Pokédex: </h2>
+    <p>Busque seus pokemons preferidos!</p>
+      <section className="boxBarra">
         <input className="pesquisar" value={busca} onChange={(e) => setBusca(e.target.value)} placeholder="Insira ID ou nome de um pokémon"/>
+        <button className="botaoBusca" onClick={guardarInformacoes}> Buscar </button>
+        <p>{mensagem}</p>
       </section>
     </>
   )
