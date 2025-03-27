b78c5f2dfaeb7530d77c8dc0b41a11c23cdf13c1
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-03-27 14:47:26 -0300

    Favoritos funcionando

--- src/pages/Favoritos.jsx
+++ src/pages/Favoritos.jsx
@@ -1,5 +1,5 @@
 // Hooks
-
+    import { useState } from "react"
 //.
 
 // Style
@@ -14,16 +14,31 @@
 //.
 
 function Favoritos() {
+    const [infoFavorito] = useState(JSON.parse(localStorage.getItem("Favoritos")) || []);
+
+    const removerFavorito = (numeroArray) => {
+        const novoFavoritos = infoFavorito.filter((conteudo, index) => index !== numeroArray)
+        return (localStorage.setItem("Favoritos", JSON.stringify(novoFavoritos)))
+    }
+
     return(
         <>
             <section className="bodyFavoritos">
                 <section>
-                    <Header />
+                    <Header />\
                     <Navbar favoritos="ativo" />
                 </section>
                 <section className="mainFavoritos">
                     <h1>Favoritos</h1>
-
+                    <section className="infoFavoritos">
+                        {infoFavorito && infoFavorito > 0 && infoFavorito.map((pokemon, index) => (
+                            <section className="cartaFavoritos">
+                                <img src={pokemon.imagem} alt={`Imagem do ${pokemon.nome} `} />
+                                <h2>{pokemon.nome}</h2>
+                                <button onClick={removerFavorito(index)}>Desfavoritar</button>
+                            </section>
+                        ))}
+                    </section>
                     
                 </section>
                 <Footer />
