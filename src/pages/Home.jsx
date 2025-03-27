768922eea416d52ca488daa8e6900eef4b4cad4b
Milena <mile.26souza08@gmail.com>
2025-03-27 14:01:28 -0300

    Estilizei mais

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Navbar from "../components/NavBar"
+import "../components/styles/NavBar.css"
 
 
 function Home() {
     return(
         <>
-            <Navbar />
+            <Navbar home="ativo" />
         </>
     )
 }
