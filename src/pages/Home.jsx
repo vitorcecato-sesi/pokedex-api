bce20992a9e374d5f82c5d82a6a56764266da11a
Lucas <lucascasagrandesesi@gmail.com>
2025-04-03 01:28:22 -0300

    Correção no sistema de busca e melhoria no css do botao

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,85 +1,94 @@
-import { useEffect, useState } from "react"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import NavBar from "../components/NavBar"
-import TrocaDeCor from "../components/botaoTema"
-import Carta from "../components/Carta"
+import { useEffect, useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+import TrocaDeCor from "../components/botaoTema";
+import Carta from "../components/Carta";
 
-import "./styles/Home.css"
-
-import Carta from "../components/Carta"
+import "./styles/Home.css";
 
 function Home() {
-
-  const [pokemons, setPokemons] = useState(JSON.parse(localStorage.getItem("Dados API")) || {})
-  const [busca, setBusca] = useState("")
-  const [informacoes, setInformacoes] = useState(JSON.parse(localStorage.getItem("Informacoes")) || "")
-  const [erro, setErro] = useState(false)
+  const [pokemons, setPokemons] = useState(JSON.parse(localStorage.getItem("Dados API")) || {});
+  const [busca, setBusca] = useState("");
+  const [buscaInput, setBuscaInput] = useState("");
+  const [informacoes, setInformacoes] = useState(JSON.parse(localStorage.getItem("Informacoes")) || {});
+  const [erro, setErro] = useState(false);
 
   useEffect(() => {
-    async function buscarPokemons() {
-      try {
-        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${busca.toLowerCase()}`)
-        const dados = await resposta.json()
-        if (dados) {
-          setPokemons(dados)
-          setErro(true)
-        } else {
-          setErro(false)
+      async function buscarPokemons() {
+        try {
+          const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${busca.toLowerCase()}`);
+          const dados = await resposta.json();
+          if (dados) {
+            setPokemons(dados);
+            setErro(false);
+            guardarInformacoes(dados);
+          } else {
+            setErro(true);
+          }
+        } catch (error) {
+          console.error(error);
+          setErro(true);
         }
-      } catch (error) {
-        console.error(error)
-        setErro(false)
       }
-    }
-    if (busca) buscarPokemons()
-  }, [busca])
+      buscarPokemons();
+    
+  }, [busca]);
 
-  const guardarInformacoes = () => {
-    const dados = {
-      nome: pokemons.name,
-      imagem: pokemons.sprites.front_default,
-      tipo: pokemons.types[0].type.name,
-      habilidades: pokemons.abilities.map(abilidade => abilidade.ability.name),
-      estatistica: pokemons.stats.map(estatistica => estatistica.base_stat)
-    }
-    setInformacoes(dados)
-    localStorage.setItem("Informacoes", JSON.stringify(informacoes))
-    localStorage.setItem("Informacoes", JSON.stringify(informacoes))
-  }
+  const guardarInformacoes = (dados) => {
+    const informacoes = {
+      nome: dados.name,
+      imagem: dados.sprites.front_default,
+      tipo: dados.types[0].type.name,
+      habilidades: dados.abilities.map(abilidade => abilidade.ability.name),
+      estatistica: dados.stats.map(estatistica => estatistica.base_stat),
+    };
+    setInformacoes(informacoes);
+    localStorage.setItem("Informacoes", JSON.stringify(informacoes));
+  };
 
   useEffect(() => {
-    localStorage.setItem("Dados API", JSON.stringify(pokemons))
-  }, [pokemons])
+    localStorage.setItem("Dados API", JSON.stringify(pokemons));
+  }, [pokemons]);
+
+  const clicarBotao = () => {
+    if (buscaInput == "") { 
+      setErro(true)
+      return
+    }
+    setBusca(buscaInput)
+    setBuscaInput("")
+    setErro(false)
+  }
 
   return (
     <>
-    <Header/>
-    <NavBar/>
+      <Header/>
+      <NavBar/>
+      <section className="box-home">
       <section className="box-Barra">
-        <input className="pesquisar" value={busca} onChange={(e) => setBusca(e.target.value)} placeholder="Insira ID ou nome de um pokémon" />
-        <button className="botaoHome" onClick={guardarInformacoes}>Buscar</button>
-        <Carta/>
-
+        <input
+          className="pesquisar"
+          value={buscaInput}
+          onChange={(e) => setBuscaInput(e.target.value)}
+          placeholder="Insira ID ou nome de um pokémon"
+        />
+        <button className="botaoHome" onClick={clicarBotao}>🔍</button>
       </section>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <center>
-      <center>
+      <br/>
+      <br/>
         {erro ? (
-          <Carta />
+          <p className="erro">Por favor, insira um Pokémon válido!</p>
         ) : (
-          <p>Pokémon não encontrado</p>
+          <Carta/>
         )}
-      </center>
-      <TrocaDeCor/>
-      <br></br>
-      </center>
+        <br/>
+        <TrocaDeCor/>
+        <br/>
+      </section>
       <Footer/>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
